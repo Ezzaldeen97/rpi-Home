354616c7fb32535436cb63dbe76cf7ee3e56c5f7
Ezzaldeen97 <alribiezzaldeen@gmail.com>
2025-09-11 09:01:11 +0100

    renamed the get_info file

--- fetch_rpi_metrics.py
+++ fetch_rpi_metrics.py
@@ -3,6 +3,7 @@ import shutil
 import time, psutil
 
 def get_cpu_temp():
+    "Read the RPI CPU tempretuate"
     try:
         with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
             temp = int(f.read().strip()) / 1000.0
@@ -12,15 +13,18 @@ def get_cpu_temp():
 
 
 def get_storage():
+    "Read the RPI Total, used, free Storage"
     total, used, free = shutil.disk_usage("/")
     return f"{total//(2**30)} GB"  ,f"{used//(2**30)} GB", f"{free//(2**30)} GB"
 
 
 def get_runtime():
+    "Get the runtime of the RPI"
     boot_time = psutil.boot_time()
     return  f"{round((time.time() - boot_time))} seconds"
 
 def get_network_traffic():
+    "Get the actual Network traffic (tx, rx)"
     with open("/sys/class/net/eth0/statistics/tx_bytes", "r") as f:
         upload_traffic = round(int(f.read().strip())/1000,2)
     with open("/sys/class/net/eth0/statistics/rx_bytes", "r") as f:
