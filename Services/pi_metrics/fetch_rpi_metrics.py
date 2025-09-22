17c805e0cee9e5aed992870c7576d7cf6c25fa99
Ezzaldeen97 <alribiezzaldeen@gmail.com>
2025-09-22 08:42:03 +0100

    added logging to the fetch_rpi_metrics

--- Services/pi_metrics/fetch_rpi_metrics.py
+++ Services/pi_metrics/fetch_rpi_metrics.py
@@ -1,29 +1,40 @@
-import os
+import os,sys
 import shutil
 import time, psutil
 
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+from utils.logger import get_logger
+
+
+logger = get_logger("rpi_meta_data", file_name='logs/rpi_meta_data.log')
+
 def get_cpu_temp():
-    "Read the RPI CPU tempretuate"
+    "Read the RPI CPU temperature"
     try:
         with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
             temp = int(f.read().strip()) / 1000.0
         return f"{temp:.1f}"
     except Exception as e:
-        return f"Error: {e}"
+        logger.ERROR(f"Could not read the cpu temperature: {e}")
+        
 
 
 def get_storage():
+    #TODO: Handle errors
     "Read the RPI Total, used, free Storage"
     total, used, free = shutil.disk_usage("/")
     return f"{total//(2**30)}"  ,f"{used//(2**30)} ", f"{free//(2**30)}"
 
 
 def get_runtime():
+    #TODO: Handle errors
     "Get the runtime of the RPI"
     boot_time = psutil.boot_time()
     return  f"{round((time.time() - boot_time))} "
 
 def get_network_traffic():
+    #TODO: Handle errors
     "Get the actual Network traffic (tx, rx)"
     with open("/sys/class/net/eth0/statistics/tx_bytes", "r") as f:
         upload_traffic = round(int(f.read().strip())/1000,2)
